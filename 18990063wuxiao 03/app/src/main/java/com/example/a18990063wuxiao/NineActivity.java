4a77b2944a37959fbadd29c72f5cc5981281be98
wuxiao <wuxiao0063@163.com>
2020-10-13 20:36:13 +0800

    第三次作业 按钮控制图片位置

---
+++ 18990063wuxiao 03/app/src/main/java/com/example/a18990063wuxiao/NineActivity.java
@@ -0,0 +1,14 @@
+package com.example.a18990063wuxiao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nine);
+    }
+}
