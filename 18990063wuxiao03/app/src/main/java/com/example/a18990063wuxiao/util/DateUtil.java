d517abdc9e4b828a8ad7965889ee1953ea890b5f
wuxiao <wuxiao0063@163.com>
2020-10-20 11:33:42 +0800

    第三次作业

---
+++ 18990063wuxiao03/app/src/main/java/com/example/a18990063wuxiao/util/DateUtil.java
@@ -0,0 +1,21 @@
+package com.example.a18990063wuxiao.util;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+public class DateUtil {
+
+    public static String getNowDateTime() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+
+        return sdf.format(new Date());
+
+    }
+    public static String getNowTime() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+        return sdf.format(new Date());
+
+    }
+}
